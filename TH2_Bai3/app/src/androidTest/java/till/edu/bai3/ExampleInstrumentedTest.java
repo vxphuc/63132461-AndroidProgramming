84483c8d61fe1085f446d23b7b63ef997b0b3f70
vxphuc <phuc2652003@gmail.com>
2024-03-08 12:54:22 +0700

    update

--- TH2_Bai3/app/src/androidTest/java/till/edu/bai3/ExampleInstrumentedTest.java
+++ TH2_Bai3/app/src/androidTest/java/till/edu/bai3/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package till.edu.th2_bai3;
+package till.edu.bai3;
 
 import android.content.Context;
 
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("till.edu.th2_bai3", appContext.getPackageName());
+        assertEquals("till.edu.bai3", appContext.getPackageName());
     }
 }
