39a43ac14a1eafc34099313d5ab826f3edb84528
vxphuc <phuc2652003@gmail.com>
2024-03-26 00:03:10 +0700

    update bai 2

--- Cau2_QuanLyLopHoc/app/src/main/java/ntu/mssv63132461/bai2_quanlylophoc/MainActivity.java
+++ Cau2_QuanLyLopHoc/app/src/main/java/ntu/mssv63132461/bai2_quanlylophoc/MainActivity.java
@@ -1,24 +1,36 @@
 package ntu.mssv63132461.bai2_quanlylophoc;
 
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText edtml, edttl, edtcs;
+    Button bti, btd, btu, btq;
+    ListView lv;
+    ArrayList<String> mylist;
+    ArrayAdapter<String> myadapter;
+    SQLiteDatabase mydatabase;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        edtml = findViewById(R.id.edtml);
+        edttl = findViewById(R.id.edttl);
+        edtcs = findViewById(R.id.edtcs);
+        bti = findViewById(R.id.bti);
+        btu = findViewById(R.id.btu);
+        btd = findViewById(R.id.btd);
+        btq = findViewById(R.id.btq);
+        lv = findViewById(R.id.lv);
+        mylist = new ArrayList<>();
     }
 }
