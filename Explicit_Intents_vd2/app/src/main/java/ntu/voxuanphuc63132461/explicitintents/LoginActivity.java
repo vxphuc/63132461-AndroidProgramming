5c8e575b673517680aa0555f9aae3c22c552de5b
vxphuc <phuc2652003@gmail.com>
2024-04-20 23:46:41 +0700

    update
    hoàn thành bài vd 2 ạ

--- Explicit_Intents_vd2/app/src/main/java/ntu/voxuanphuc63132461/explicitintents/LoginActivity.java
+++ Explicit_Intents_vd2/app/src/main/java/ntu/voxuanphuc63132461/explicitintents/LoginActivity.java
@@ -1,6 +1,11 @@
 package ntu.voxuanphuc63132461.explicitintents;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +14,29 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class LoginActivity extends AppCompatActivity {
-
+    Button btnOk;
+    EditText edtName, edtPass, edtMail;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        btnOk = findViewById(R.id.btnOk);
+        edtName = findViewById(R.id.edtName);
+        edtPass = findViewById(R.id.edtPass);
+        edtMail = findViewById(R.id.edtMail);
+        btnOk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = edtName.getText().toString();
+                String password = edtPass.getText().toString();
+                String gmail = edtMail.getText().toString();
+                if (username.equals("phuc123") && password.equals("123") && gmail.equals("phuc@123.com")) {
+                    Intent myintent = new Intent(LoginActivity.this, ClientActivity.class);
+                    startActivity(myintent);
+                } else {
+                    Toast.makeText(LoginActivity.this, "Đăng nhập không thành công", Toast.LENGTH_SHORT).show();
+                }
+            }
         });
     }
 }
