4ae5024e8bfc25545ec9aab84f654f7f40326c2f
vxphuc <phuc2652003@gmail.com>
2024-04-26 14:46:10 +0700

    update

--- BottomNavigationView/app/src/main/java/ntu/voxuanphuc63132461/bottomnavigationview/MainActivity.java
+++ BottomNavigationView/app/src/main/java/ntu/voxuanphuc63132461/bottomnavigationview/MainActivity.java
@@ -7,18 +7,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
     }
 }
