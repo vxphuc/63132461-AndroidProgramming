3b991469240b7abe1c76e6130447b7efabdc021c
vxphuc <phuc2652003@gmail.com>
2024-05-10 12:57:58 +0700

    update
    em update lai baif navigation menu ạ

--- NavigationDrawler/app/src/main/java/ntu/voxuanphuc63132461/navigationdrawler/MainActivity.java
+++ NavigationDrawler/app/src/main/java/ntu/voxuanphuc63132461/navigationdrawler/MainActivity.java
@@ -1,24 +1,33 @@
 package ntu.voxuanphuc63132461.navigationdrawler;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 public class MainActivity extends AppCompatActivity {
-
+    DrawerLayout drawerLayout;
+    ImageButton btdrawertoggel;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+
+        drawerLayout = findViewById(R.id.drawerLayout);
+        btdrawertoggel = findViewById(R.id.btdrawertoggel);
+
+        btdrawertoggel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawerLayout.open();
+            }
         });
     }
 }
